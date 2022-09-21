180276fed99feeb1599078e5828570931674c432
BT_4N2_24 <rkong2588@bths.edu>
2022-09-21 12:27:50 -0400

    Initialized all the variables for user input and calculations

--- src/billCalculator.java
+++ src/billCalculator.java
@@ -2,5 +2,20 @@ import java.util.Scanner;
 public class billCalculator {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        // Variables
+            // User Input Variables
+        int numberOfPeople;
+        double totalBill;
+        int tipPercentage;
+
+            // Calculation Variables
+        double tipPercentageConverted;
+        double totalTip;
+        double totalTipPerPerson;
+        double totalBillPerPerson;
+        double totalBillRounded;
+        double totalBillAndTip;
+        double totalPerPerson;
     }
 }
