a985f9e23c92e33f48fc93d37fd73bf3e37a0b68
Tiffany <rkong2588@bths.edu>
2022-09-28 02:00:43 -0400

    reformatted math.ceil to accurately round and truncate during calculations

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -32,9 +32,11 @@ public class TipCalculator {
         tipPercentageConverted = tipPercentage * 0.01;
         totalTip = tipPercentageConverted * totalBill;
                 // round the total tip up to nearest cent
-        totalTipPerPerson = Math.ceil((totalTip / numberOfPeople) * 100);
-                // cast as integer to remove anything after two decimals
+        totalTipPerPerson = (totalTip / numberOfPeople) * 1000;
+                // cast as integer to truncate anything after three decimal places
         totalTipPerPerson = (int)totalTipPerPerson;
+                // divide by 10.0 to double and allow to round up if leftover decimal
+        totalTipPerPerson = Math.ceil(totalTipPerPerson / 10.0);
                 // reset total tip to double with cents
         totalTipPerPerson = totalTipPerPerson / 100.00;
 
@@ -42,9 +44,11 @@ public class TipCalculator {
 
             // Bill
                 // round the total bill up to nearest cent
-        totalBillPerPerson = Math.ceil((totalBill / numberOfPeople) * 100);
-                // cast as an integer to remove anything after two decimals
+        totalBillPerPerson = (totalBill / numberOfPeople) * 1000;
+                // cast as an integer to truncate anything after three decimal places
         totalBillPerPerson = (int)totalBillPerPerson;
+                // divide by 10.0 to double and allow to round up if leftover decimal
+        totalBillPerPerson = Math.ceil(totalBillPerPerson / 10.0);
                 // reset total bill to double with cents
         totalBillPerPerson = totalBillPerPerson / 100.00;
 
